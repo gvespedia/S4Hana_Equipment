90b99a164079e6f6c0171bb47ee982937bbde8e5
P2000114125 <giovanni.visona@espedia.it>
2018-04-26 09:09:40 +0000

    Working Operations Table

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -21,7 +21,20 @@ sap.ui.define([
 			var orderToolbar = this.byId("orderToolbar");
 			orderToolbar.bindElement(sPath + "/ToOrder");
 			var operationsTable = this.byId("operationsTab");
-			operationsTable.bindElement(sPath + "/ToOrder" + "/ToOperation");
+			var myPath = sPath + "/ToOperations";
+
+			var oTemplate = new sap.m.ColumnListItem({
+					type: sap.m.ListType.Inactive,
+					cells: [
+						new sap.m.ObjectIdentifier({title:"{Description}", text: "{Activity}"}),
+						new sap.m.Label({text: "{WorkCntr}"})
+					]
+				});
+				//var oTemplate = operationsTable.getBindingInfo("items").template;
+			//operationsTable.bindItems({path: myPath });
+			operationsTable.bindAggregation("items", myPath, oTemplate);
+
+			//operationsTable.bindElement(sPath + "/ToOperations");
 			//header.setBindingContext(new sap.ui.model.Context(oModel, "/BapiNotifSet('10000143')/ToEqui"));
 			//header.setTitle("{path:'Qmnum'}");
 		},
